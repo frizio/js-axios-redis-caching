368d2d2ee76caf6c06a5475a84bee025d1450f6c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-01-31 19:45:53 +0100

    Use axios to call remote rickandmortyapi url

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const axios = require("axios");
+const redis = require("redis");
+
+// Rick and Morty API Base URL
+const BASE_URL = "https://rickandmortyapi.com/api";
+
 router.get('/', (req, res) => {
   res.json({'version': '1.1.0'});
+});
+
+router.get('/character', async(req, res) => {
+  // Fetching Data
+  const response = await axios.get(BASE_URL + "/character");
+  console.log(response.data);
+  res.json([]);
+
 });
 
 module.exports = router;
